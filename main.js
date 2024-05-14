7d8ff00f4b2a775a373894d86c1bf89a558dd09a
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2024-05-14 12:41:11 +1000

    refine a simpler ui and logic

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-const shareButton1 = document.querySelector(".share-button-1");
+const shareButton = document.querySelector(".share-button");
 const shareButton2 = document.querySelector(".share-button-2");
 const authorWrapper = document.querySelector(".author-wrapper");
-const shareWrapper = document.querySelector(".toggle-share-wrapper");
+const authorInfoWrapper = document.querySelector(".author-info-wrapper");
+const shareWrapper = document.querySelector("#share-wrapper");
 
-shareButton1.addEventListener("click", () => {
-  shareWrapper.classList.remove("toggle-share-wrapper");
-  authorWrapper.style.display = "none";
-});
-
-shareButton2.addEventListener("click", () => {
-  shareWrapper.classList.add("toggle-share-wrapper");
-  authorWrapper.style.display = "flex";
+shareButton.addEventListener("click", () => {
+  if (shareWrapper.style.display === "flex") {
+    shareWrapper.style.display = "none";
+    authorWrapper.style.background = "none";
+    authorInfoWrapper.style.display = "flex";
+    shareButton.classList.add("share-button");
+    shareButton.classList.remove("share-button-2");
+  } else {
+    shareWrapper.style.display = "flex";
+    authorInfoWrapper.style.display = "none";
+    authorWrapper.style.background = "var(--very-dark-grayish-blue)";
+    shareButton.classList.remove("share-button");
+    shareButton.classList.add("share-button-2");
+  }
 });
